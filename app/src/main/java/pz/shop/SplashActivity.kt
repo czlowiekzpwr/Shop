b1d8449a384dd2d4516e6c17a3f2de182f0838f5
Kareshis <franek.stepek@gmail.com>
2021-10-28 19:46:50 +0200

    Added simple working Loading Activity

---
+++ app/src/main/java/pz/shop/SplashActivity.kt
@@ -0,0 +1,27 @@
+package pz.shop
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    //loading time of the splash screen
+    private val splashTimeOut:Long = 3000 // 1 sec
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this,MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, splashTimeOut)
+    }
+}
